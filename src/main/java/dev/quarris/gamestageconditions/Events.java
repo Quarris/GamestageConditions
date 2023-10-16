869f33fc2bfef0a32b1e94f6b262caaf6deb9ef5
ProbablyNotPetey <petertvedt01@gmail.com>
2023-10-16 14:28:43 -0400

    1.19.2 update

--- src/main/java/dev/quarris/gamestageconditions/Events.java
+++ src/main/java/dev/quarris/gamestageconditions/Events.java
@@ -10,15 +10,15 @@ public class Events {
 
     @SubscribeEvent
     public static void on(GameStageEvent.Added event) {
-        if (event.getPlayer() instanceof ServerPlayer) {
-            GamestageConditions.GAMESTAGE_ADDED_TRIGGER.trigger((ServerPlayer) event.getPlayer(), event.getStageName());
+        if (event.getEntity() instanceof ServerPlayer) {
+            GamestageConditions.GAMESTAGE_ADDED_TRIGGER.trigger((ServerPlayer) event.getEntity(), event.getStageName());
         }
     }
 
     @SubscribeEvent
     public static void on(GameStageEvent.Removed event) {
-        if (event.getPlayer() instanceof ServerPlayer) {
-            GamestageConditions.GAMESTAGE_REMOVED_TRIGGER.trigger((ServerPlayer) event.getPlayer(), event.getStageName());
+        if (event.getEntity() instanceof ServerPlayer) {
+            GamestageConditions.GAMESTAGE_REMOVED_TRIGGER.trigger((ServerPlayer) event.getEntity(), event.getStageName());
         }
     }
 }
